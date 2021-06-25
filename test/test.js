35534301ada4efcd6780595b9bafebba976cf663
OceanRX <76732468+OceanRX@users.noreply.github.com>
2021-06-25 20:20:41 +0300

    corona-info

--- test/test.js
+++ test/test.js
@@ -1,12 +1,12 @@
 const covid = require('../index');
 
 async function consoleWrite() {
-    let res = await covid.find({ country: "all" });
+    let res = await covid.findData({ country: "all" });
     console.log(res)
 }
 
 async function countryWrite() {
-    let res = await covid.find({ country: "USA"})
+    let res = await covid.findData({ country: "USA"})
     console.log(res)
 }
 
